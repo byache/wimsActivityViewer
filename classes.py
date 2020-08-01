51f853df98cfeee7b7562cbe967f3ceec6a6289d
byache <paul@byache.fr>
2020-08-01 22:28:27 +0200

    scores et autres...

--- classes.py
+++ classes.py
@@ -3,34 +3,38 @@
 
 from time import mktime
 
-
 # cette classe gère les données des lignes du fichier de log "scores"
 #extrait de ce genre de fichier :
-#20191215.19:17:52 RH225FD237 28  1 renew  	109.0.207.75	181276269	noscore
-#20191215.19:17:59 RH225FD237 28  1 score 0  	109.0.207.75		noscore
-#20191215.19:18:09 RH225FD237 28  1 new  	109.0.207.75	181613962
-#20191215.19:18:13 RH225FD237 28  1 score 10  	109.0.207.75
+#20191215.19:17:52 RH225FD237 28  1 renew   109.0.207.75    181276269   noscore
+#20191215.19:17:59 RH225FD237 28  1 score 0     109.0.207.75        noscore
+#20191215.19:18:09 RH225FD237 28  1 new     109.0.207.75    181613962
+#20191215.19:18:13 RH225FD237 28  1 score 10    109.0.207.75
 class LigneLog:
     # chaque ligne de log devient un objet
     def __init__(self, lineraw):
         # ligne raw designe la ligne "brute"
         lineraw = lineraw + '\t'
         lineraw = lineraw.split('\t')
+        lineraw=[x for x in lineraw if x] #pour enlever tout les éléments "falsy" du genre '' ou encore []
+
         self.sc = True
         if("noscore" in lineraw):
             self.sc = False
         #self.IP = lineraw[1] on n'enregistre pas l'ip par souci de confidentialité
         lineraw = lineraw[0].split(' ')
+        lineraw=[x for x in lineraw if x] #pour enlever tout les éléments "falsy" du genre '' ou encore []
+
         dateraw = lineraw[0].split('.')
         dateraw2 = dateraw[0]
         self.date = dateraw2[6:8] + "/" + dateraw2[4:6] + \
-					"/" + dateraw2[0:4]
+                    "/" + dateraw2[0:4]
         self.timetext = dateraw[1]
         timeraw = dateraw[1].split(':')
         structime = int(dateraw2[0:4]),int(dateraw2[4:6]),int(dateraw2[6:8]),int(timeraw[0]),int(timeraw[1]),int(timeraw[2]),0,1,-1
         self.time = mktime(structime)
         self.session = lineraw[1]
         self.sheet = int(lineraw[2])
+        
         self.exercise = int(lineraw[3])
         self.type = lineraw[4]
         self.score = 0
--- wav.py
+++ wav.py
@@ -8,7 +8,7 @@ from shutil import rmtree
 from flask import Flask, flash, request, redirect, url_for, render_template, session, send_from_directory
 from werkzeug.utils import secure_filename
 from io import BytesIO
-from data_factory import * #contient des fonctions enlevées d'ici pour ne pas surcharger...
+from datafactory import * #contient des fonctions enlevées d'ici pour ne pas surcharger...
 from uuid import uuid4
 
 UPLOAD_FOLDER = 'tmp'
@@ -20,98 +20,108 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 #max size 10 Mo
 
 def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def timeverif(duration):
-	#retourne true si la durée entre l'heure enregistrée dans la session et l'heure actuelle est supérieure à duration
-	return false
-
-def logout():
-	#vide les cookies de session
-	nothing = ''
-	
+    
 def cleanData(session):
-	#supprime le dossier de la session 'session'
-	rmtree(os.path.join(app.config['UPLOAD_FOLDER'], session))
+    #supprime le dossier de la session 'session'
+    rmtree(os.path.join(app.config['UPLOAD_FOLDER'], session))
+    #os.rmdir(os.path.join(app.config['UPLOAD_FOLDER'], session))
 
 def cleanAllData(duration=3600):
-	#supprime tous les fichiers de 'tmp' non modifiés depuis 'duration' secondes
-	t_limite = time() - duration
-	sessions = os.listdir(app.config['UPLOAD_FOLDER'])
-	for s in sessions:
-		if os.path.getmtime(os.path.join(app.config['UPLOAD_FOLDER'], s)) < t_limite :
-			cleanData(s)
+    #supprime tous les fichiers de 'tmp' non modifiés depuis 'duration' secondes
+    t_limite = time() - duration
+    sessions = os.listdir(app.config['UPLOAD_FOLDER'])
+    for s in sessions:
+        if os.path.getmtime(os.path.join(app.config['UPLOAD_FOLDER'], s)) < t_limite :
+            cleanData(s)
 
 @app.route('/tmp/<session>/<titre>')
 def download_file(session,titre):
-	uploads = os.path.join(app.config['UPLOAD_FOLDER'], session)
-	return send_from_directory(directory=uploads, filename=titre)
+    uploads = os.path.join(app.config['UPLOAD_FOLDER'], session)
+    return send_from_directory(directory=uploads, filename=titre)
 
 @app.route('/', methods=['GET', 'POST'])
 def main_function():
-	# on crée le répertoire tmp s'il n'existe pas déjà
-	os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-	# on nettoie les sessions trop vieilles (+ de 1h)
-	cleanAllData(3600)
-	
-	# on regarde si c'est la première page qui est vue dans la session
-	if 'pseudo' not in session :
-		# si oui, on attribue un numéro aléatoire unique de session et on le stocke dans le cookie 'pseudo'
-		pseudo = uuid4().hex
-		session['pseudo'] = pseudo
-		#session['page'] = "accueil"
-		#on crée un répertoire avec le numéro de session dans 'tmp'
-		dirname = secure_filename(pseudo)
-		dirpath = os.path.join(app.config['UPLOAD_FOLDER'], dirname)
-		os.mkdir(dirpath,0o755)
+    # on crée le répertoire tmp s'il n'existe pas déjà
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    # on nettoie les sessions trop vieilles (+ de 1h)
+    cleanAllData(3600)
+    
+    # on regarde si c'est la première page qui est vue dans la session
+    if 'pseudo' not in session :
+        # si oui, on attribue un numéro aléatoire unique de session et on le stocke dans le cookie 'pseudo'
+        pseudo = uuid4().hex
+        session['pseudo'] = pseudo
+        #session['page'] = "accueil"
+        #on crée un répertoire avec le numéro de session dans 'tmp'
+        dirname = secure_filename(pseudo)
+        dirpath = os.path.join(app.config['UPLOAD_FOLDER'], dirname)
+        os.mkdir(dirpath,0o755)
 
-		
-	pseudo = session['pseudo']
-	dirname = secure_filename(pseudo)
-	dirpath = os.path.join(app.config['UPLOAD_FOLDER'], dirname)
-	#crée le répertoire s'il n'existe pas déjà (inutile à priori, mais bon... 
-	os.makedirs(dirpath, exist_ok=True)
-	zipname = os.path.join(dirpath,'class.zip')
-	#succession des vues : ["accueil","feuille","result"]
-	try:
-		page = request.form['page']
-	except:
-		page = 'accueil'
-	feuilles = []
-		
-	#if page == 'accueil':
-	#	session['page'] = "feuille"
-	#el
-	if request.method == 'POST' and page == 'feuille':
-		# check if the post request has the file part          -------> ne marche pas !!
-		if 'file' not in request.files and not os.path.isfile(zipname):
-			flash('Aucun fichier sélectionné')
-			#session['page'] = "accueil"
-			return redirect(request.url)
-		if 'file' not in request.files and os.path.isfile(zipname):
-			feuilles = fsheets(zipname)
-		if 'file' in request.files:
-			file = request.files['file']
-			# if user does not select file, browser also
-			# submit an empty part without filename
-			if file.filename == '':
-				flash('Aucun fichier sélectionné 2')
-				#session['page'] = "accueil"
-				return redirect(request.url)
-			#on vérifie que c'est un zip
-			if not allowed_file(file.filename):
-				flash('Le fichier sélectionné doit être une archive zip.')
-				#session['page'] = "accueil"
-				return redirect(request.url)
-			if file and allowed_file(file.filename):
-				file.save(zipname)
-				#return data_factory(BytesIO(file.read()),f)
-				feuilles = fsheets(zipname)
-				#session['page'] = 'result'
-	elif page == 'result':
-		feuille = int(request.form['feuille'])
-		return data_factory(zipname,feuille,dirpath)
-	return render_template('accueil.html', page=page, feuilles=feuilles)
+        
+    pseudo = session['pseudo']
+    dirname = secure_filename(pseudo)
+    dirpath = os.path.join(app.config['UPLOAD_FOLDER'], dirname)
+    #crée le répertoire s'il n'existe pas déjà (inutile à priori, mais bon... )
+    os.makedirs(dirpath, exist_ok=True)
+    zipname = os.path.join(dirpath,'class.zip')
+    #succession des vues : ["accueil","feuille","result"]
+    try:
+        page = request.form['page']
+    except:
+        page = 'accueil'
+    feuilles = []
+    
+    if request.method == 'POST' and page == 'feuille':
+        # check if the post request has the file part          -------> ne marche pas !!
+        if 'file' not in request.files and not os.path.isfile(zipname):
+            flash('Aucun fichier sélectionné')
+            #session['page'] = "accueil"
+            return redirect(request.url)
+        if 'file' not in request.files and os.path.isfile(zipname):
+            try:
+                feuilles = fsheets(zipname)
+            except:
+                flash("Votre zip ne semble pas être une sauvegarde de classe Wims. Erreur de fichier ?")
+                return redirect(request.url)
+        if 'file' in request.files:
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('Aucun fichier sélectionné 2')
+                #session['page'] = "accueil"
+                return redirect(request.url)
+            #on vérifie que c'est un zip
+            if not allowed_file(file.filename):
+                flash('Le fichier sélectionné doit être une archive zip.')
+                #session['page'] = "accueil"
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                file.save(zipname)
+                try:
+                    feuilles = fsheets(zipname)
+                except:
+                    flash("Votre zip ne semble pas être une sauvegarde de classe Wims. Erreur de fichier ?")
+                    return redirect(request.url)
+    elif page == 'logout':
+        cleanData(pseudo)
+        session.pop('username', None)
+        flash("Vos données ont été supprimées du serveur. Au revoir et à bientôt.")
+        return redirect(request.url)
+    elif page == 'result':
+        feuille = int(request.form['feuille'])+1
+        dtfct = data_factory(zipname,feuille,dirpath)
+        if dtfct[0]=='error':
+            flash(dtfct[1])
+            return redirect(request.url)
+        else:
+            return dtfct
+    return render_template('accueil.html', page=page, feuilles=feuilles)
 
 application = app
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
